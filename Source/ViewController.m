cc4e2a59770d60d7133cf2e1da52af818889497b
Thomas Tempelmann <tempelmann@gmail.com>
2019-03-03 14:45:31 +0100

    Now browses actual images.
    Performance sucks, though, because it appears that the earlier (and visible) items do not get rendered before all the other (even still invisible) items have generated their previews as well.
    
    When resizing the window to a smaller size and then maximizing it, all images get re-fetched again, causing another delay that should not be necessary.
    
    The end result is that this method, using a QLPreviewView in every Collection Item is not quite efficient when browsing large amounts of files with previews.

--- Source/ViewController.m
+++ Source/ViewController.m
@@ -9,27 +9,49 @@
 #import "MyPreviewView.h"
 #import "MyPreviewItem.h"
 
+
+@interface ViewController()
+	@property (strong) NSMutableArray<NSMutableDictionary*> *contents;
+@end
+
+
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
+
+	self.contents = [NSMutableArray new];
+	NSString *dirPath = @"/Library/Desktop Pictures";
+	NSFileManager *fm = [NSFileManager defaultManager];
+	NSDirectoryEnumerator *dirEnum = [fm enumeratorAtPath:dirPath];
+	NSString *fileName;
+	int i = 0;
+	while ((fileName = [dirEnum nextObject])) {
+		NSURL *url = [NSURL fileURLWithPath:[dirPath stringByAppendingPathComponent:fileName]];
+		if (url.hasDirectoryPath) {
+			[dirEnum skipDescendents];
+		} else {
+			++i;
+			NSMutableDictionary *item = [NSMutableDictionary dictionary];
+			item[@"itemTitle"] = [NSString stringWithFormat:@"%d: %@", i, fileName];
+			item[@"itemURL"] = url;
+			[self.contents addObject:item];
+		}
+	}
+
 	[self.collectionView registerClass:[MyCollectionViewItem class] forItemWithIdentifier:@"MyCollectionViewItem"];
 	self.collectionView.dataSource = self;
 }
 
 - (NSInteger)collectionView:(NSCollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-	return 1000;
+	return self.contents.count;
 }
 
 - (NSCollectionViewItem *)collectionView:(NSCollectionView *)collectionView itemForRepresentedObjectAtIndexPath:(NSIndexPath *)indexPath
 {
+	NSInteger fileIdx = [indexPath indexAtPosition:1];
 	MyCollectionViewItem *item = [collectionView makeItemWithIdentifier:@"MyCollectionViewItem" forIndexPath:indexPath];
-	item.textField.stringValue = [NSString stringWithFormat:@"Idx %ld (#%ld)", [indexPath indexAtPosition:1], item.serialNum];
-
-	MyPreviewItem *previewItem = [MyPreviewItem new];
-	NSURL *url = [NSURL fileURLWithPath:@"/System"];
-	previewItem.previewItemURL = url;
-	previewItem.previewItemTitle = item.textField.stringValue;
+	item.textField.stringValue = [NSString stringWithFormat:@"Idx %ld (#%ld)", fileIdx, item.serialNum];
 
 	QLPreviewView *qlView;
 	if (item.qlViewContainer.subviews.count == 0) {
@@ -42,6 +64,13 @@
 		qlView = item.qlViewContainer.subviews[0];
 		assert(qlView.previewItem==nil);
 	}
+
+	NSDictionary *content = self.contents[fileIdx];
+	item.representedObject = content;
+
+	MyPreviewItem *previewItem = [MyPreviewItem new];
+	previewItem.previewItemURL = content[@"itemURL"];
+	previewItem.previewItemTitle = content[@"itemTitle"];
 	qlView.previewItem = previewItem;
 
 	return item;
